300c77fde71cd07d8c3b2314f052f2baf7d8f8b9
wj0911 <itsdaduck@gmail.com>
2025-07-22 15:05:24 -0400

    Removed an extra unneeded line in test-sample.sql for advanced feature 1

--- sql/test-sample.sql
+++ sql/test-sample.sql
@@ -187,9 +187,6 @@ SELECT * FROM Recipes r WHERE EXISTS (SELECT Recipe_ID FROM Cooklist_Recipes WHE
 -- INSERT INTO Cooklists (User_ID, Name, Description, Is_Public) VALUES (6, 'Liked Recipes', 'All your liked recipes in one place!', TRUE);
 
 -- advanced feature 1: 
--- inserting extra ingredients so that user 2 has enough to make recipe 1
-INSERT INTO User_Ingredients (User_ID, Ingredient_ID, Quantity) VALUES (2, 2, 2);
-
 -- creating view (if you're user 2)
 CREATE VIEW My_Ingredients AS
 SELECT i.Ingredient_ID, i.Name, ui.Quantity
